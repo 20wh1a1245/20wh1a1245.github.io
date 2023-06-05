6e08c66a83a87806d1995fc8072f9e9194943647
20wh1a1245 <93665824+20wh1a1245@users.noreply.github.com>
2023-06-05 21:29:21 +0530

    added functionality for button

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("This is a progress tracker");
+var data1 = {image : "images/childhood.jpg", name : "Meenakshi Supriya Elluri", designation : "Learner"};
+var data2 = {image : "images/latest.jpg", name : "Meenakshi Supriya", designation : "Intern"};
+var isData1 = true;
+var displayData;
+var changeMyPic = function()
+{
+    if(isData1)
+    {
+        displayData = data2;
+        isData1 = false;
+    }
+    else
+    {
+        displayData = data1;
+        isData1 = true;
+    }
+}
